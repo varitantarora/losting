b82605d553a3ec9d497c9824e046a878988f821a
varitant <varitanta@gmail.com>
2023-11-30 00:28:13 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -7,17 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  
-    next();
-  });
+app.use(cors({origin:'http://localhost:3000',credentials:true}));
 dotenv.config();
 connectDatabase();
 
