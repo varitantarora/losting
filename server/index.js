4dca2278796b2260d0e7631c304bccfce2738edf
varitant <varitanta@gmail.com>
2023-11-30 00:22:53 +0530

    cors fixed

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:'http://localhost:3000',credentials:true}));
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
